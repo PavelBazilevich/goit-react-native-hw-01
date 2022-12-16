423a793b66d32b715f643ad41d7d20f94a00fe4a
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-12-16 21:05:27 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, this is my first project on react-native! 😊</Text>
+      <Text style={styles.title}>
+        Hello, this is my first project on react-native! 😊
+      </Text>
+      <TextInput style={styles.input} placeholder="Type text" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#2196f3",
+    backgroundColor: "#eaeaea",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    paddingVertical: 8,
+    borderWidth: 2,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#2196f3",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderRadius: 6,
+    borderWidth: 2,
+    width: 200,
+    padding: 5,
+    margin: 10,
   },
 });
